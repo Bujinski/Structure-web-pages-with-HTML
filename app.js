f6d2a427098c21b7a208f6e253cce37b42a7892d
Darya Buzhynskaya <Bujinski4u@gmail.com>
2023-08-28 18:05:13 +0000

    save JS

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 
 alert("Welcome to the store!");
 
+
+function getAnswer(){
 const userAnswer = prompt("Looking for a great book?");
-console.log(userAnswer);
+return userAnswer; 
+}
+
+let theirAnswer = getAnswer(); 
+
+console.log(theirAnswer);
 
 //string concatenation
 document.write("Welocome to the store!");
 
-if (userAnswer == "yes") {
+if (theirAnswer == "yes") {
     alert("Great! Let's find the best one!");
 } else { 
     alert("No problem, let's find something interesting for you!");
@@ -18,12 +25,12 @@ const userLoc = prompt("Do you know where is our location?");
 console.log(userLoc);
 
 if (userLoc == "yes") {
-    alert("Wonderful, let's see what books do we have!");
+    alert("Wonderful, let's see what books we have!");
 } else { 
     alert("No problem, you can find it by clicking the location link.");
 }
 
-const userFBook = prompt("Have you checked our books of the months?");
+const userFBook = prompt("Have you checked our books of the month?");
 console.log(userFBook);
 
 if (userFBook == "yes") {
@@ -31,4 +38,20 @@ if (userFBook == "yes") {
 } else { 
     alert("Great, let's check them now!");
 }
+
+  // ---------------------------------
+
+  // FUNCTIONS //
+
+function addTwoNumbers() {
+    let num1 = 7;
+    let num2 = 4;
+    let sum = num1 + num2;
+    console.log(sum);
+    return sum; 
+}
+
+// Invoking it, or calling it
+
+addTwoNumbers();
 
